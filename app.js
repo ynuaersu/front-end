58a8c7b0413a989e4e2afac0787ebb3b5cb9284f
Youness HAKIM <younesshakim19@gmail.com>
2019-06-30 16:47:41 +0100

    fixing app.js

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 var d  = new Date();
+var page = 1;
 let year = d.getFullYear();
-let month = d.getMonth() - 1;
-let day = d.getDay().toString();
+let month = d.getMonth();
+let day = d.getDate().toString();
 if(d.getDay() == 31){
 	day = '30';
-}else if(d.getDay()<10){
+}else if(d.getDate()<10){
 	day = '0' + day;
 }
 if(d.getMonth()<10){
 	month = '0' + month;
 }
 
-
-alert('https://api.github.com/search/repositories?q=created:<'+year+'-'+month+'-'+day+'&sort=stars&order=desc');
 const vrep = new Vue({
     el: '#container',
     data: {
       items: []
     },
     mounted() {
-      axios.get('https://api.github.com/search/repositories?q=created:<'+year+'-'+month+'-'+day+'&sort=stars&order=desc')
+      axios.get('https://api.github.com/search/repositories?q=created:>'+year+'-'+month+'-'+day+'&sort=stars&order=desc&page='+page)
               .then(res => {this.items = res.data.items});
-    }, 
+    },
 });

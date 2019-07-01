74f038ba56127b332f39f46710a95418955ad49c
Youness HAKIM <younesshakim19@gmail.com>
2019-07-01 16:22:15 +0100

    Loading new repositories

--- app.js
+++ app.js
@@ -11,14 +11,52 @@ if(d.getDay() == 31){
 if(d.getMonth()<10){
 	month = '0' + month;
 }
-
 const vrep = new Vue({
     el: '#container',
     data: {
-      items: []
+      items: [],
+			loading: false
     },
     mounted() {
+			this.loading = true;
       axios.get('https://api.github.com/search/repositories?q=created:>'+year+'-'+month+'-'+day+'&sort=stars&order=desc&page='+page)
-              .then(res => {this.items = res.data.items});
+              .then(res => {
+								this.loading = false;
+								this.items = res.data.items;
+							}, (error)  =>  {
+				        this.loading = false;
+				      });
     },
 });
+//alert('https://api.github.com/search/repositories?q=created:>'+year+'-'+month+'-'+day+'&sort=stars&order=desc&page='+page);
+var l = true;
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        page++;
+				var container = document.getElementById('container');
+
+				if(l){
+					container.innerHTML+='<div v-if="loading" class="loading"><img src="img/loading.gif" alt=""></div><div class="repos" v-for="item in items"><span class="user_avatar"><img :src=\'item.owner.avatar_url + ".png"\'></span><span class="name"><h1>{{item.name}}</h1></span><span class="desc"><p>{{item.description}}</p></span><span class="stars">Stars: {{item.stargazers_count}}</span><span class="issues">Issues: {{item.open_issues_count}}</span><span class="user"><i>Submitted {{item.pushed_at}} ago by {{item.owner.login}}</i></span></div>';
+					l = false;
+					new Vue({
+					    el: '#container',
+					    data: {
+					      items: [],
+								loading: false
+					    },
+					    mounted() {
+								this.loading = true;
+					      axios.get('https://api.github.com/search/repositories?q=created:>'+year+'-'+month+'-'+day+'&sort=stars&order=desc&page='+page)
+					              .then(res => {
+													this.loading = false;
+													l = true;
+													this.items = res.data.items;
+												}, (error)  =>  {
+									        this.loading = false;
+									      });
+					    },
+					});
+				}
+
+    }
+};
